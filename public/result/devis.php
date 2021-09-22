3bb8bedda7c110b900e223e571975e8a8f0bf8ab
Carlo <ccarlo@hotmail.it>
2021-09-22 16:51:03 +0200

    small fixes

--- public/result/devis.php
+++ public/result/devis.php
@@ -31,7 +31,7 @@ require_once (__DIR__ . '/../includes/header.php');
 
 <section id="home">
 	<div class="home-bg">
-		<a href="#"><img src="<?=$domain?>/img/banners/Profil_ 1980x714 px.png" alt=""></a>
+		<img src="<?=$domain?>/img/banners/Profil_ 1980x714 px.png" alt="">
 	</div>
 </section>
 
