ca6fb471715d746b72b5d63cdf139194c59924b2
Christo26.g <Christo26.g@gmail.com>
2021-09-28 15:46:46 +0200

    change

--- public/db/global.php
+++ public/db/global.php
@@ -6,7 +6,7 @@ define('DB_USER', 'christo1_christopher26');
 define('DB_PASSWORD','r93F3.]JxRx6Gt');
 define('DB_HOST', 'localhost');
 
-/* define('DB_NAME', 'formablocs');
+/*define('DB_NAME', 'formablocs');
 define('DB_USER', 'root');
 define('DB_PASSWORD','');
 define('DB_HOST', 'localhost'); */
